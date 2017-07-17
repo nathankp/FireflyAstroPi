114f290be96fa87402a291ffd585bdfdac85f11e
root <bazookaharwell@gmail.com>
2017-07-16 16:35:17 -1000

    Changed time format on output files to (YY/MM/DD HH:MM:SS) and added this format as a column in output files

--- autoWeather.py
+++ autoWeather.py
@@ -28,19 +28,21 @@ def export_data(temp,hum,pres,pos):
 # the appropriate value to the `values` variable.
 def main():
     sense = SenseHat()
-    sense.clear() 
+    sense.clear()
     # the logging interval (in seconds).
-    interval = 1
+    interval = 600
     # a generator which produces the target time of
     # the next reading.  Change its argument to whatever
     # interval you want to log at.
     schedule = make_schedule(interval)
     # shortcut to generate current time.
-    now = lambda: int(time.time())  
+    now = lambda: int(time.time())
+    # timestamp formatted
+    nowFormat = lambda: datetime.datetime.fromtimestamp(now()).strftime('%Y-%m-%d %H:%M:%S')
     # list of headers for the csv file.
     headers = ["temp","hum","pres","pos"]
     # use start-time in filename to prevent collisions.
-    filename = "output-{}.csv".format(now())
+    filename = "output-{}.csv".format(nowFormat())
     # open file inside `with` statement to ensure that
     # file is automatically closed when program ends.
     with open(filename,'w') as fp:
@@ -51,21 +53,21 @@ def main():
         # begin main loop.
         for delta in schedule:
             # wait the `delta` between now and next reading time.
-            if delta > 0.001: 
+            if delta > 0.001:
                 time.sleep(delta)
             # -------- BEGIN CORE PROGRAM LOGIC --------
-         
+
             # write the actual logic if your program here,
             # resulting in a list name `values`.
             temp = sense.get_temperature()
             hum = sense.get_humidity()
             pres = sense.get_pressure()
             pos = sense.get_compass_raw()
-            values = export_data(temp,hum,pres,pos) 
+            values = export_data(temp, hum, pres, pos)
             # --------- END CORE PROGRAM LOGIC ---------
             # write the values to the target file, appending
             # the current time as the far right column.
-            writer.writerow((values,now()))
+            writer.writerow((values, now(), nowFormat()))
             # force python3 to actually write the value immediately
             # instead of buffering (so we don't lose data when pgrm dies).
             fp.flush()
