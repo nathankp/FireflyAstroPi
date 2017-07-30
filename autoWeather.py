a58465dbb78c8edd29f6cb24f005fe5298b7c770
Nate <park.nathan@gmail.com>
2017-07-30 09:02:18 +0000

    revised 'autoWeather.py' data collection script. Changed the file name format back to unix time to avoid special characters such as '""' and ' '. These characters make it very difficult to open and move files.

--- autoWeather.py
+++ autoWeather.py
@@ -2,8 +2,11 @@
 
 import time
 import csv
+import datetime
 from itertools import count
 from sense_hat import SenseHat
+sense = SenseHat()
+sense.clear()
 
 # NOTE: To get this script to run startup, add it to `/etc/rc.local`.
 
@@ -17,8 +20,8 @@ def make_schedule(interval):
     return sched
 
 def export_data(temp,hum,pres,pos):
-    x,y,z = [pos.get(k) for k in ['x','y','z']]
-    text = "{}, {}, {}, {}, {}, {}".format(temp,hum,pres,x,y,z)
+ #   x,y,z = [pos.get(k) for k in ['x','y','z']]
+    text = "{}, {}, {}".format(temp,hum,pres)
     return text
 
 
@@ -27,29 +30,28 @@ def export_data(temp,hum,pres,pos):
 # want, and add whatever core logic you need in order to assign
 # the appropriate value to the `values` variable.
 def main():
-    sense = SenseHat()
-    sense.clear()
     # the logging interval (in seconds).
-    interval = 600
+    interval = 3
     # a generator which produces the target time of
     # the next reading.  Change its argument to whatever
     # interval you want to log at.
     schedule = make_schedule(interval)
     # shortcut to generate current time.
-    now = lambda: int(time.time())
+    now = lambda: int(time.time())-36000 #adjusting to hawaii time
+   # strnow = str(now)
     # timestamp formatted
-    nowFormat = lambda: datetime.datetime.fromtimestamp(now()).strftime('%Y-%m-%d %H:%M:%S')
+    nowFormat = lambda: datetime.datetime.fromtimestamp(now()).strftime('"%Y-%m-%d %H:%M:%S"')
     # list of headers for the csv file.
-    headers = ["temp","hum","pres","pos"]
+    headers = ["temp","hum","pres","headers"]
     # use start-time in filename to prevent collisions.
-    filename = "output-{}.csv".format(nowFormat())
+    filename = "output-{}.csv".format(now())
     # open file inside `with` statement to ensure that
     # file is automatically closed when program ends.
     with open(filename,'w') as fp:
         # generate a csv writer obect.
         writer = csv.writer(fp)
         # write the headers to the file.
-        writer.writerow((headers, "timestamp"))
+        writer.writerow((headers))
         # begin main loop.
         for delta in schedule:
             # wait the `delta` between now and next reading time.
@@ -62,12 +64,16 @@ def main():
             temp = sense.get_temperature()
             hum = sense.get_humidity()
             pres = sense.get_pressure()
-            pos = sense.get_compass_raw()
-            values = export_data(temp, hum, pres, pos)
+            list_temp = repr(temp)
+            list_hum = repr(hum)
+            list_pres = repr(pres)
+            dataL = [list_temp,list_hum,list_pres]
+           # pos = sense.get_compass_raw()
+           # values = export_data(temp, hum, pres)
             # --------- END CORE PROGRAM LOGIC ---------
             # write the values to the target file, appending
             # the current time as the far right column.
-            writer.writerow((values, now(), nowFormat()))
+            writer.writerow((dataL,nowFormat()))
             # force python3 to actually write the value immediately
             # instead of buffering (so we don't lose data when pgrm dies).
             fp.flush()
